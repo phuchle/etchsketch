2836cf3b88d6755c210960eb9f3eaa81ddb4d843
Phuc H. Le <le.phuc.h@gmail.com>
2016-01-17 23:18:20 -0800

    figured out why new grid wasn't created with inputted values: needed to change the global values from inside function via omitting var from prompt

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
 //todo: figure out why rows aren't being created properly when reset --changed to display block
 //why is hovering code for new grid broken? --added code to 
 //get the width and height of squares to fill #wrapper
-//
+// //
 
-var rows = 100; //define how many rows and columns in your grid
-var columns = 100; //define how many rows and columns in your grid
+var rows = 5; //define how many rows and columns in your grid
+var columns = 5; //define how many rows and columns in your grid
 // var pixel = 1000 / rows + 'px';
 
+
+$(document).ready(function() { 
+
+
+
+	// var rows = prompt("Please enter the length of your grid", "Please input a number");
+	// var columns = prompt("Please enter the width of your grid", "Please input a number");
+
+	makeGrid();
+
+	document.getElementById("reset").onclick = function() {
+		$(".square").removeClass("blackBackground");
+		$(".square").remove();
+		$(".row").remove();
+	
+		rows = parseInt(prompt("Please enter the length of your new grid", "Please input a number"), 10);
+		columns = parseInt(prompt("Please enter the width of your new grid", "Please input a number"), 10);
+
+		makeGrid();
+	};	
+
+	document.getElementById("clear").onclick = function() {
+		$(".square").removeClass("blackBackground");
+	};
+});
+
+
+
 function makeGrid() {
+	// var $wrapper = $("<div />", {
+	// 	class: 'wrapper'
+	// });
+
 	var $row = $("<div />", { // make the jQuery variable row and make it a <div class="row">
 		class: 'row'
 	});
@@ -17,53 +49,17 @@ function makeGrid() {
 	});
 
 	for (var i = 0; i < columns; i++) { //create squares in the row
-			$row.append($square.clone());
+		$row.append($square.clone());
 	};
 
 	for (var i = 0; i < rows; i++) { //add more rows to the wrapper
-		$("#wrapper").append($row.clone());
+		$('#wrapper').append($row.clone());
 	};
 
-	// function changeSquare() { // modify square to fit wrapper
-	// 	var elements = document.getElementsByClass('square');
-	// 	for (var i = 0; i < elements.length; i++) {
-	// 		$('.square').css({
-	// 			'height': pixel,
-	// 			'width': pixel 
-	// 		});
-	// };
+	var width = ($('#wrapper').width())/ rows;
+	$('.square').css({"width": width, "height": width});
 
-	$(".square").mouseover(function() {
+	$('.square').mouseover(function() {
 		$(this).addClass('blackBackground');
 	});
 };
-
-$(document).ready(function() { 
-
-	makeGrid();
- 	
-	document.getElementById("reset").onclick = function() {
-
-		$(".square").removeClass("blackBackground");
-		$(".square").remove();
-		$("row").remove();
-		
-		var rows = prompt("Please enter the length of your new grid", "Please input a number");
-		var columns = prompt("Please enter the width of your new grid", "Please input a number");
-
-		
-
-		// if (isnaN(squaresPerSide)) {
-		// 	alert("Please enter a number");
-		// 	var squaresPerSide = prompt("How many squares per side would you like in your new grid?", "Please input a number");
-		// }
-		
-		makeGrid();
-	};	
-
-	document.getElementById("clear").onclick = function() {
-		$(".square").removeClass("blackBackground");
-	};
-
-	
-});
